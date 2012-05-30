94acc502f488664d72192a46a3897453888c6617
Gabriel Blondeau <blondeau.gabriel@gmail.com>
2012-05-30 14:48:44 +0200

    Profil

--- profil.php
+++ profil.php
@@ -11,28 +11,48 @@
             </ul>
             <div class="tab-content" style="padding-bottom: 9px; border-bottom: 1px solid rgb(221, 221, 221);">
                 <div class="tab-pane active" id="tab1">
-                    <form class="form-horizontal">
+                    <form class=" well form-horizontal">
                         <fieldset>
                             <div class="control-group">
-                                <label class="control-label" for="input01"><b>Pseudo</b></label>
+                                <label class="control-label" for="inputpseudo"><b>Pseudo</b></label>
                                 <div class="controls">
-                                    <span class="input-xlarge uneditable-input">Tom</span>
+                                    <span class="input-small uneditable-input">Tom</span>
                                 </div> 
                                 
-                                <label class="control-label" for="fileInput"><b>Adresse&nbsp;e-mail</b></label>
+                                <label class="control-label" for="inputemail"><b>Adresse&nbsp;e-mail</b></label>
                                 <div class="controls">
-                                    <input type="text" class="input-xlarge" id="inputmail" value="adresse@mail.com" type="text">
+                                    <input type="text" class="input-medium" id="inputemail" placeholder="Email">
                                     <button type="submit" class="btn btn-primary">Enregistrer</button>
                                 </div>
                                 
                                 <label class="control-label"><b>Changer&nbsp;de&nbsp;mot&nbsp;de&nbsp;passe&nbsp;:*</b></label>
-                                <a class="btn btn-primary" href="./modifmdp.php">Modifier</a>
+                                <div class="controls">
+                                    <a class="btn btn-primary" href="./modifmdp.php">Modifier</a>
+                                </div>
                             </div>
                         </fieldset>
                     </form>
                 </div>
                 <div class="tab-pane" id="tab2">
-
+                    <div class="control-group">
+                        <div class="controls">
+                            <h4>Image actuelle</h4>
+                            <ul class="thumbnails">
+                                <li class="span3">
+                                <a href="#" class="thumbnail">
+                                    <img src="http://fc03.deviantart.net/fs71/f/2011/098/f/e/trollface_wallpaper_by_juliothechange-d3dhjlw.jpg" alt="">
+                                </a>
+                                </li>   
+                            </ul>
+                        </div>
+                        <div class="controls">
+                            <label class="control-label" for="Upluader une image">Upluader un Fichier</label>
+                            <div class="controls">
+                                <input class="input-file" id="fileInputimage" type="file">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Enregistrer</button>
+                        </div>
+                    </div>
                 </div>
             </div>
             
