c1ba3b3f17c686dca1f3982a6585688ea0314216
Gabriel Blondeau <blondeau.gabriel@gmail.com>
2012-05-30 09:41:21 +0200

    modifmdp - modifié

--- modifmdp.php
+++ modifmdp.php
@@ -84,44 +84,38 @@
       
     
       
-      
-    <div class="form-actions">      
-        <label class="control-label"><b>Mot&nbsp;de&nbsp;passe&nbsp;actuel&nbsp;:*</b></label>
-        <div class="controls">
-            <input type="text" class="input-xlarge" id="inputtactuel" value="" type="text">
+      <div class="container">
+        <div class="form-actions">      
+            <label class="control-label"><b>Mot&nbsp;de&nbsp;passe&nbsp;actuel&nbsp;:*</b></label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="inputtactuel" value="" type="text">
+            </div>
         </div>
-    </div>
- 
+      </div>
       
       
       
       
-<div class="form-actions">
-    <label class="control-label"><b>Nouveau&nbsp;mot&nbsp;passe&nbsp;:*</b></label>
-    <div class="controls">
-        <input type="text" class="input-xlarge" id="inputtnouveau1" value="" type="text">
-    </div>
-    <label class="control-label"><b>Confirmation&nbsp;du&nbsp;nouveau&nbsp;mot&nbsp;passe&nbsp;:*</b></label>
-    <div class="controls">
-        <input type="text" class="input-xlarge" id="inputtnouveau2" value="" type="text">
-    </div>
-    <div class="container">
-        <a class="btn btn-primary" href="#">Confirmer</a>
-        <a class="btn btn-inverse" href="./modifmdp.php">Réinitialiser</a>
-    </div>
-</div>
+      <div class="container">  
+        <div class="form-actions">
+            <label class="control-label"><b>Nouveau&nbsp;mot&nbsp;passe&nbsp;:*</b></label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="inputtnouveau1" value="" type="text">
+            </div>
+            <label class="control-label"><b>Confirmation&nbsp;du&nbsp;nouveau&nbsp;mot&nbsp;passe&nbsp;:*</b></label>
+            <div class="controls">
+                <input type="text" class="input-xlarge" id="inputtnouveau2" value="" type="text">
+            </div>
+            <div class="container">
+                <a class="btn btn-primary" href="#">Confirmer</a>
+                <a class="btn btn-inverse" href="./modifmdp.php">Réinitialiser</a>
+            </div>
+        </div>
+      </div>
       
       
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
+            
       
       
       
