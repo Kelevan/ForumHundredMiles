5877f2a6e6200c717bb5c45a5a4db12529a1a2ae
Vincent Jamet <vincent.jamet45@gmail.com>
2012-05-30 09:50:02 +0200

    modif includes

--- sendmessage.php
+++ sendmessage.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-<!--------------------------------HEAD------------------------------------->    
-  <head>
-    <meta charset="utf-8">
-    <title>Hundred Miles</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="./css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-    </style>
-    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
-    
+<?php
+    include_once("./includes.php");
+    entete("Administration");
+?>   
     <!--------- Editeur de text -------->
     <script type="text/javascript" src="./js/tiny_mce/tiny_mce.js"></script>
     <script type="text/javascript">
@@ -49,58 +29,6 @@
   </head>
   
 <!--------------------------------BODY------------------------------------->  
-  <body>
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="./index.php">Hundred Miles</a>
-          <div class="btn-group pull-right">
-            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
-              <i class="icon-user"></i> Username
-              <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Profil</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Se déconnecter</a></li>
-            </ul>
-          </div>
-          <div class="nav-collapse">
-            <ul class="nav">
-              
-                <div class="tabbable">
-                <ul class="nav nav-pills">
-                <li><a href="./index.php">Home</a></li>
-                <ul class="nav nav-pills">
-                <li class="dropdown">
-                <a class="dropdown-toggle"
-                data-toggle="dropdown"
-                href="#">
-                Catégories
-                <b class="caret"></b>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="#tab2">Organisation</a></li>
-                    <li><a href="./categorie.php">Musiques</a></li>
-                    <li><a href="#tab2" data-toggle="tab">Concerts</a></li>
-                    <li><a href="#tab2" data-toggle="tab">Equipe</a></li>
-                    <li><a href="#tab2" data-toggle="tab">Groupes</a></li>
-                    <li><a href="#tab2" data-toggle="tab">Support</a></li>
-                </ul>
-                </li>
-                </ul>
-                </ul>
-                </div>
-                
-          </div>
-        </div>
-      </div>
-    </div>
     <div class="container">
         <ul class="breadcrumb">
         <li><a href="./index.php">Home</a> <span class="divider">/</span></li>
@@ -129,36 +57,15 @@
                 <div class="form-actions">
                     <div class="row">
                         <div class="span1"> <button type="submit" class="btn btn-success">Répondre</button></div>
-                        <div class="span1"> <button class="btn btn-info">Aperçue</button></div>
+                        <div class="span1"> <button class="btn btn-info">Aperçu</button></div>
                         <div class="span1"> <button class="btn btn-inverse">Annuler</button></div>                    
                     </div>
                 </div>     
             </div>  
          </div>
+<?php
+    pied();
+?>              
      </fieldset>
-      
-      
-      
-<!--------------------------------FOOTER------------------------------------->  
-   <div class="navbar navbar-fixed-bottom">
-     <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="right" href="#">By Kelevan & Kirby</a>
-        </div>
-      </div>
-   </div>
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./js/jquery.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
- 
-
   </body>
 </html>       
