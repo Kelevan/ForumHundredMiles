9d7738766f967bd2576b8e2ba8b1daab2bf6956d
Gabriel Blondeau <blondeau.gabriel@gmail.com>
2012-05-31 13:54:22 +0200

    dewplayr

--- index.php
+++ index.php
@@ -5,37 +5,18 @@
     <div class="container">
       <div class="row">  
 <!----------- Tab FICHIERS ----------->         
-        <div class="span6">
+        <div class="span3">
             <legend>Derniers&nbsp;Fichiers</legend>
             
-            <object type="application/x-shockwave-flash" data="./dewplayer/dewplayer-playlist.swf" width="200" height="20" id="dewplayer" name="dewplayer-playlist">
-                <param name="movie" value="dewplayer.swf" />
-                <param name="flashvars" value="mp3=./audio/SleepAway.mp3" />
+            <object type="application/x-shockwave-flash" data="./dewplayer/dewplayer-playlist.swf" width="200" height="20" id="dewplayer" name="dewplayer">
                 <param name="wmode" value="transparent" />
+                <param name="movie" value="dewplayer.swf" />
+                <param name="flashvars" value="showtime=true&autoreplay=true&xml=./dewplayer/playlist.xml" />                
             </object>
-            
-            <!--<table class="table table-bordered table-condensed">
-                <thead>
-                    <tr class="row">
-                        <th colspan="4"><h4>Nom&nbsp;du&nbsp;fichier</h4></th>
-                        <th colspan="4"><h4>Topic</h4></th>
-                        <th colspan="2"><h4>Propriétaire</h4></th>
-                        <th colspan="2"><h4>Types</h4></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="row">
-                        <td colspan="4"><a href="./audio/SleepAway.mp3">2 see</a></td>
-                        <td colspan="4">Lyrics</td>
-                        <td colspan="2">Val</td>
-                        <td colspan="2">Audio</td>
-                    </tr>
-                </tbody>
-            </table> -->
         </div>
 
 <!----------- Tab Topics/Messages ----------->
-          <div class="span6">
+          <div class="span9">
             <legend>Dernières&nbsp;news</legend>
             <table class="table table-bordered table-condensed">
                 <thead>
