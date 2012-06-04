5371ee753d9fcf955592fd88e4ca2083b816762d
Vincent Jamet <vincent.jamet45@gmail.com>
2012-06-04 16:08:35 +0200

    new topic

--- newTopic.php
+++ newTopic.php
@@ -4,31 +4,24 @@
 ?> 
 
 <div class="container">     
-    <form method="post" action="somepage" class="well">
+    <form method="post" action="somepage" class="well form-horizontal">
         <div class="control-group">
             <label class="control-label" for="input01"><b>Titre du topic</b></label>
             <div class="controls">
                 <input type="text" class="input-medium" id="input01">  
             </div>
         </div>
-        <textarea name="content" style="width:50%"></textarea>            
         <div class="control-group">
-            <label class="control-label" for="Upluader un Fichier">Uploader&nbsp;un&nbsp;Fichier</label>
-            <div class="controls">
-                <input class="input-file" id="fileInput" type="file">
-            </div> 
-        </div>  
+        <label class="control-label" for="input02">
+        <textarea name="content" style="width:50%" id="input02"></textarea>  
+        </label>
+        </div>      
         <div class="form-actions">
-            <div class="row">
-                <div class="span1"> <button type="submit" class="btn btn-success">Envoyer</button></div>
-                <div class="span1"> <button type="reset" class="btn btn-warning">Annuler</button></div>                    
-            </div>
+            <button type="submit" class="btn btn-success">Envoyer</button>
+            <button type="reset" class="btn btn-warning">Annuler</button>                   
         </div>
     </form>
-      
-
-
 
 <?php
-    pied();
+    pied();;
 ?>   
