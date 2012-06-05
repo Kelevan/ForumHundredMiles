75a59b031facbe3ea529ba94e213397b6a9dfda8
Vincent Jamet <vincent.jamet45@gmail.com>
2012-06-05 11:41:40 +0200

    popo

---
+++ connect.php
@@ -0,0 +1,43 @@
+<?php
+	include_once("./includes.php");
+	if(isset($_GET['action']))
+	{
+		switch($_GET['action'])
+		{
+			case "logout":
+				// SCRIPT DE DECONNEXION
+				entete("Deconnexion...");
+				$_SESSION = array();
+				echo "Vous etes deconnecte... Renvoi vers l'accueil.";
+				pied();
+				die('<meta http-equiv="refresh" content="2;URL=./index.php">');				
+				break;
+			case "login":
+				entete("Connexion...");
+				if (empty($_POST['mail'])or empty($_POST['pass']))
+				{
+					echo "Vous avez oublie de remplir un champ";
+					die('<meta http-equiv="refresh" content="2;URL=./index.php">');
+				}	
+				$req_connexion=query("select * from utilisateur where mail='".$_POST['mail']."'; ");
+				if(mysql_num_rows($req_connexion)==0)
+				{
+					echo "E_mail incorrect";
+					die('<meta http-equiv="refresh" content="2;URL=./index.php">');
+				}	
+				$connexion=mysql_fetch_array($req_connexion);
+			 	if(($_POST['pass'])!=$connexion['password'])
+				{
+					echo "Mot de passe incorrect";
+					die('<meta http-equiv="refresh" content="2;URL=./index.php">');
+				}	
+				//$_SESSION['idUtil']=$connexion['idUtil'];
+				//$_SESSION['TypeAdh']=$connexion['TypeAdh'];
+				//$_SESSION['NomCompletAdh']=$connexion['PrenomAdh']." ".$connexion['NomAdh'];
+				echo "Connexion reussie... Renvoi vers l'accueil.";
+				pied();
+				die('<meta http-equiv="refresh" content="2;URL=./index.php">');
+				break;
+		}
+	}	
+?>
