333b93d97289f26fda0f6c86b34ae2e40e442c5f
Gabriel Blondeau <blondeau.gabriel@gmail.com>
2012-06-07 19:08:30 +0200

    listefofo

--- listeForum.php
+++ listeForum.php
@@ -26,27 +26,37 @@
                     $req_connexion=query("select * from forum");
                     while ($row = mysql_fetch_array($req_connexion, MYSQL_NUM)) {
                         echo "<tr class='row'><td>".$row[0]."</td>
-                            <td>".$row[1]."</td>
-                                ".$idforumpere=$req_connexion('row[3]');"
-                            <td>".$result=query("select nomForum from forum where idForum=".$idforumpere); $resultidforumpere=$result; $resultidforumpere."</td>
-                            <td><a class='btn' href='./editForum.php?id=".$row[0]."'><i class='icon-pencil'></i></a>
-                            <div id='SuppressionMembre' class='modal hide fade'>
-                                <div class='modal-header'>
-                                    <button class='close' data-dismiss='modal'>&times;</button>
-                                    <h3>Suppression d'un utilisateur</h3>
-                                </div>
-                                <div class='modal-body'>
-                                    <h5>Etes-vous sur de vouloir supprimer l'utilisateur KirbyFr ?</h5>    
-                                </div>
-                                <div class='modal-footer'>
-                                    <a href='#' class='btn btn-primary'>Oui</a>
-                                    <a href='#' class='btn btn-warning data-dismiss=modal'>Non</a>
-                                </div>
+                            <td>".$row[1]."</td>";
+                        $result=query("select nomForum from forum where idForum=".$row[3]);
+                        
+                        if(mysql_num_rows($result)==0)
+                        {
+                            $nomForumPère = "";
+                        }else {
+                            $forumpere=mysql_fetch_array($result);
+                            $nomForumPère = $forumpere[0];
+                        }
+                        
+                         
+                        echo"<td>".$nomForumPère."</td>
+                        <td><a class='btn' href='./editForum.php?id=".$row[0]."'><i class='icon-pencil'></i></a>
+                        <div id='SuppressionMembre' class='modal hide fade'>
+                            <div class='modal-header'>
+                                <button class='close' data-dismiss='modal'>&times;</button>
+                                <h3>Suppression d'un utilisateur</h3>
                             </div>
-                            <a data-toggle='modal' href='#' class='btn btn-danger'>
-                                <i class='icon-remove'></i>
-                            </a></td>
-                            </tr>";
+                            <div class='modal-body'>
+                                <h5>Etes-vous sur de vouloir supprimer l'utilisateur KirbyFr ?</h5>    
+                            </div>
+                            <div class='modal-footer'>
+                                <a href='#' class='btn btn-primary'>Oui</a>
+                                <a href='#' class='btn btn-warning data-dismiss=modal'>Non</a>
+                            </div>
+                        </div>
+                        <a data-toggle='modal' href='#' class='btn btn-danger'>
+                            <i class='icon-remove'></i>
+                        </a></td>
+                        </tr>";
 
                     }
                 ?>
