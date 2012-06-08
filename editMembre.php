15cae0aa090c764beb14b51df371f42d228d1e71
kirbyfr <blondeau.gabriel@gmail.com>
2012-06-08 10:58:06 +0200

    formulaire edit membre en relation avec listemembre

--- editMembre.php
+++ editMembre.php
@@ -13,30 +13,25 @@
             <h3>Modifier un membre</h3><br/>
                 <div class="tab-pane active" id="tab1">
                     <form class="well form-horizontal" method="POST" action="transfert_editMembre.php">
-                        <fieldset>
-                            <?php
-                                if(isset($_GET['id'])){
-                                    echo $_GET['id'];
-                                }
-                            ?>
-                            <div class="control-group">
-                                <label class="control-label" for="select01"><b>Numéro du membre</b></label>
-                                <div class="controls">
-                                    <?php
-                                        $req_connexion=query("select (idUtil) from utilisateur");
-                                    ?>
-                                </div>
-                            </div>                         
+                        <fieldset>                                                  
                             <div class="control-group">
                                 <label class="control-label" for="input01"><b>Pseudo</b></label>
                                 <div class="controls">
-                                    <input type="text" class="input-medium" id="pseudo" name="pseudo">  
+                                    <?php 
+                                         if(isset($_GET['id'])){                                
+                                            $util_connexion=query("select * from utilisateur where idUtil=".$_GET['id']."");
+                                            $row=mysql_fetch_array($util_connexion);
+                                        }
+                                        echo"<input type='text' class='input-medium' id='pseudo' name='pseudo' value='".$row[1]."'>"; 
+                                    ?> 
                                 </div>
                             </div>
                             <div class="control-group">
                                 <label class="control-label" for="input02"><b>E-mail</b></label>
                                 <div class="controls">
-                                    <input type="email" class="input-medium" id="mail" name="mail">  
+                                    <?php
+                                        echo"<input type='email' class='input-medium' id='mail' name='mail' value='".$row[3]."'>";
+                                    ?>
                                 </div>
                             </div> 
                             <div class="control-group">
@@ -44,10 +39,9 @@
                                 <div class="controls">
                                 <select id="select02">
                                     <?php
-                                        $req_connexion=query("select * from type_utilisateur");
-                                        while ($row = mysql_fetch_array($req_connexion, MYSQL_NUM)) {
+                                        $typeutil_connexion=query("select * from type_utilisateur");
+                                        while ($row = mysql_fetch_array($typeutil_connexion, MYSQL_NUM)) {
                                         echo "<option value='".$row[0]."'>".$row[1]."</option>";
-
                                         }
                                     ?>
                                 </select>
@@ -56,13 +50,13 @@
                             <div class="control-group">
                                 <label class="control-label"><b>Mot&nbsp;de&nbsp;passe&nbsp;:*</b></label>   
                                 <div class="controls">
-                                    <input type="password" class="input-xlarge" id="pass" name="pass">
+                                    <input type='password' class='input-xlarge' id='pass' name='pass'>
                                 </div>
                             </div>
                             <div class="control-group">                                                       
                                 <label class="control-label"><b>Confirmation:*</b></label>
                                 <div class="controls">
-                                    <input type="password" class="input-xlarge" id="pass2" name="pass2">
+                                    <input type='password' class='input-xlarge' id='pass2' name='pass2'>
                                 </div>
                             </div>
                             <div class="form-actions">
