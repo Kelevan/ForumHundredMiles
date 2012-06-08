37c7a8f468c7b982527cc4d09329ae539e278c7d
Vincent Jamet <vincent.jamet45@gmail.com>
2012-06-08 10:59:09 +0200

    youhouuu

--- deletecategorie.php
+++ deletecategorie.php
@@ -1,3 +1,15 @@
 <?php
-echo $_GET['id'];
+    include_once("./db.php");
+    
+    echo "Vous avez supprime le membre numero ".$_GET['id']."";
+    if (isset($_GET['id']))  
+    {
+        query("DELETE FROM categorie WHERE idCat=" .$_GET['id']."");
+        die('<meta http-equiv="refresh" content="2;URL=./listeCategorie.php">');
+    }
+    else
+    {
+        echo"marche pas";
+    }
+    
 ?>
