5bcad7f95207d9989a1c2f184814a7cc7894d12d
Vincent Jamet <vincent.jamet45@gmail.com>
2012-06-11 13:39:43 +0200

    momo

--- categorie.php
+++ categorie.php
@@ -13,6 +13,7 @@
         </ul>
     </div> 
     <div class="container">
+        <form class="well form-horizontal" method="post">        
     <table class="table table-bordered table-condensed">
         <thead>
             <tr>
@@ -20,31 +21,30 @@
 
             </tr>
             <tr calss="row">
-                <th></th>
+                <th>Lu/non lu</th>
+                <th>Img forum</th>
                 <th><center>Forums </center></th>
-                <th>Sujets</th>
                 <th>Messages</th>
                 <th>Vues</th>
                 <th>Dernier message</th>
             </tr>
         </thead>
         <tbody>
-            <tr calss="row">
-                <td>logo_new</td>
-                <td>img_micro</td>
-                <td><a href="./forum.php">Lyrics</a><br/>Avancées des lyrics, idées, bout de texte, lyrics complète...</td>
-                <td>23</td>
-                <td>21</td>
-                <td>Jeu Mai 24,2012 8:50pm <br/>Tom</td>
-            </tr>
-            <tr>
-                <td>logo_new</td>
-                <td>img_ampoule</td>
-                <td><a href="#">Riffs & idées</a><br/> Tous nos riffs et idées pour les futurs tracks</td>
-                <td>11</td>
-                <td>81</td>
-                <td>Jeu Mai 24,2012 10:50pm <br/>Kev</td>
-            </tr>
+        <?php
+        $req_connexion=query("select * from forum");
+        while ($row = mysql_fetch_array($req_connexion, MYSQL_NUM)) {
+        echo "<tr class='row'>
+        <td></td>    
+        <td>".$row[1]."</td>
+        <td></td>
+        <td></td>
+        <td></td>    
+        <td></td>    
+        </tr>";
+        }
+
+        ?>
+
         </tbody>
     </table>
 <?php
