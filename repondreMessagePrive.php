ee2e24e8831f85ac7e36898510eea9f9e08d5dbd
kirbyfr <blondeau.gabriel@gmail.com>
2012-06-14 12:49:56 +0200

    mp

--- repondreMessagePrive.php
+++ repondreMessagePrive.php
@@ -1,13 +1,15 @@
 <?php
     include_once("./db.php");
       
-        if(!isset($_POST['titre'])==0)
+        if(!empty($_POST['repondre']))
         {
             query("INSERT INTO messageprive(titreMsgPrive,contenuMsgPrive,destinataireId,auteurId) VALUES('".$_POST['titre']."','".$_POST['repondre']."','".$_POST['idauteur']."','".$_POST['session']."')");
             header("location: ./messagerie.php");
         }
         else
         {
-            echo"NO ok";
+            echo"Vous n'avez pas rempli le champ de réponse";
+            $url="./messagerie.php";
+            die('<meta http-equiv="refresh" content="1;URL='.$url.'">');
         }
 ?>
--- sendMessagePrive.php
+++ sendMessagePrive.php
@@ -1,17 +1,28 @@
 <?php
     include_once("./db.php");
-      
-        if(!isset($_POST['sujet'])==0)
+    
+        $req_connexion=query("select * from utilisateur where pseudo='".$_POST['destinataire']."'; ");
+        if(mysql_num_rows($req_connexion)==0)
         {
-            
-            $idDest_connexion=query("SELECT idUtil FROM utilisateur WHERE pseudo='".$_POST['destinataire']."'");
-            $idDest=mysql_fetch_array($idDest_connexion);
-            $idDest_result=$idDest[0];
-            query("INSERT INTO messageprive(titreMsgPrive,contenuMsgPrive,destinataireId,auteurId) VALUES('".$_POST['titre']."','".$_POST['envoie']."','".$idDest_result."','".$_POST['session']."')");
-            header("location: ./messagerie.php");
-        }
-        else
+            echo'Destinataire inconnu';
+            $url="./messagerie.php";
+            die('<meta http-equiv="refresh" content="1;URL='.$url.'">');
+        }else
         {
-            echo"NO ok </br>"; echo"".$_POST['titre']."</br>";echo"".$_POST['envoie']."</br>";echo"".$idDest_result."</br>";echo"".$_POST['session']."</br>";
+            if(!empty($_POST['titre']) && !empty($_POST['envoie']))
+            {
+
+                $idDest_connexion=query("SELECT idUtil FROM utilisateur WHERE pseudo='".$_POST['destinataire']."'");
+                $idDest=mysql_fetch_array($idDest_connexion);
+                $idDest_result=$idDest[0];
+                query("INSERT INTO messageprive(titreMsgPrive,contenuMsgPrive,destinataireId,auteurId) VALUES('".$_POST['titre']."','".$_POST['envoie']."','".$idDest_result."','".$_POST['session']."')");
+                header("location: ./messagerie.php");
+            }
+            else
+            {
+                echo"Vous n'avez pas rempli tous les champs";
+                $url="./messagerie.php";
+                die('<meta http-equiv="refresh" content="1;URL='.$url.'">');
+            }
         }
 ?>
